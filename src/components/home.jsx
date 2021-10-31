1b8416c11551ad5d84bb03a06c5ddf406452ae19
pooh <cktjdgus45@naver.com>
2021-10-31 16:17:30 +0900

    firebase setup

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+const Home = (props) => {
+    const history = useHistory();
+    return (
+        <>
+            <h1>Home</h1>
+            <button onClick={() => {
+                history.push('/profile');
+            }}>go to profile</button>
+        </>
+    )
+}
+export default Home;
