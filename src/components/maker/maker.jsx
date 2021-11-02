ce407e0f3b50abaf0bafafd2e39e4f8af46a3d4d
pooh <cktjdgus45@naver.com>
2021-11-02 21:19:22 +0900

    Maker CardUI

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'paul',
+            company: 'Naver',
+            theme: 'light',
+            title: 'software Engineer',
+            email: 'cktjdgus45@naver.com',
+            message: 'try again',
+            fileName: 'paul',
+            fileUrl: ''
+        },
+        {
+            id: '2',
+            name: 'alsa',
+            company: 'kakao',
+            theme: 'dark',
+            title: 'software Engineer',
+            email: 'cktjdgus12@naver.com',
+            message: 'try again never give up',
+            fileName: 'paulasd',
+            fileUrl: ''
+        },
+        {
+            id: '3',
+            name: 'dwein',
+            company: 'Samsung',
+            theme: 'colorful',
+            title: 'software Engineer',
+            email: 'cktjdgus97@naver.com',
+            message: 'try do again',
+            fileName: 'paulqwe',
+            fileUrl: ''
+        },
+    ])
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -23,8 +58,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}></Header>
             <div className={styles.container}>
-                <Editor></Editor>
-                <Preview></Preview>
+                <Editor cards={cards}></Editor>
+                <Preview cards={cards}></Preview>
             </div>
             <Footer></Footer>
         </section>
